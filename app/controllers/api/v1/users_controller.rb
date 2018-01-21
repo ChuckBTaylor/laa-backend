287d541c4adebab7032ace9b3db6ffa39aebf12a
Will Scripps <WLScripps@gmail.com>
2018-01-20 20:07:29 -0500

    Updated User Controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,20 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-    params #{phone_number: string, state: string, district: string}
-    state = State.find_by(abbreviation: params[:state])
-    user = User.new(phone_number: params[:phone_number], state: state, district: state.getUserDistrict(params[:district]))
-    
+    user_params #{phone_number: string, state: string, district: string}
+    state = State.find_by(abbreviation: user_params[:state])
+    user = User.new(phone_number: user_params[:phone_number], state: state, district: state.getUserDistrict(user_params[:district]))
+
     user.save
     render json: user, status: 200
+  end
+
+
+  private
+
+
+  def user_params
+    params.require(:user).permit(:state, :phone_number, :district)
   end
 
 end
